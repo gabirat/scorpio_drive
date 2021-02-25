82a136497d5752b2e4a95e9439d06e126c77fbc3
gabirat <gabirat@onet.pl>
2021-02-25 15:05:59 +0100

    initial commit

---
+++ scripts/drive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+import canopen #https://github.com/christiansandberg/canopen Check for example code
+from sensor_msgs.msg import Joy
+
+class DriveModule:
+
+    def __init__(self):
+
+        #ROS Node setup
+        rospy.init_node('drive', anonymous=True)
+
+        #CANOpen setup
+        self.network = canopen.Network()
+        can_interface = rospy.get_param('~can_interface')
+        micontrol_eds = rospy.get_param('~micontrol_eds')
+        self.network.connect(channel=can_interface, bustype='socketcan')
+        self.wheels = {
+            "fl": self.network.add_node(1, micontrol_eds),
+            "fr": self.network.add_node(2, micontrol_eds),
+            "rl": self.network.add_node(3, micontrol_eds),
+            "rr": self.network.add_node(4, micontrol_eds)
+        }
+        #network.nmt.state = 'OPERATIONAL'
+
+        #ROS Node setup
+        rospy.Subscriber('joy', Joy, self.controller_data)
+        
+
+    def controller_data(self, data):
+        for wheel_key in self.wheels:
+            self.wheels[wheel_key].sdo['Velocity - desired value'].raw = data.axes[4] * 500 # 0 - 500 RPM
+
+
+if __name__ == '__main__':
+    drive = DriveModule()
+    rospy.spin()
+    drive.network.disconnect()
